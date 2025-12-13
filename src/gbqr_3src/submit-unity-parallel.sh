d9015d788ab7cc61c91c073374730cb8651ff0aa
Nicholas Reich <nick@umass.edu>
2025-12-13 12:49:23 -0500

    updates to script dates.

--- src/gbqr_3src/submit-unity-parallel.sh
+++ src/gbqr_3src/submit-unity-parallel.sh
@@ -4,70 +4,20 @@
 #SBATCH -c 8                              # Number of cores per task
 #SBATCH --mem=32G                         # Memory per node
 #SBATCH -p cpu                            # Partition name
-#SBATCH -t 02:00:00                       # Time limit (2 hours)
-#SBATCH --array=0-54                      # Array indices (55 dates total)
+#SBATCH -t 08:00:00                       # Time limit (8 hours)
+#SBATCH --array=0-3                       # Array indices (4 dates total)
 #SBATCH -o logs/slurm-%A_%a.out           # Output file (%A=job ID, %a=array index)
 #SBATCH -e logs/slurm-%A_%a.err           # Error file
 #SBATCH --mail-type=FAIL,TIME_LIMIT_80    # Email on failure or 80% time reached
 #SBATCH --mail-user=nick@umass.edu
 
-# Array of dates to process (2023-2024 and 2024-2025 seasons)
+# Array of dates to process (2025-2026 season)
+# Note: iddata NSSP loading requires as_of dates >= 2025-09-17
 dates=(
-  "2023-10-18"
-  "2023-10-25"
-  "2023-11-01"
-  "2023-11-08"
-  "2023-11-15"
-  "2023-11-22"
-  "2023-11-29"
-  "2023-12-06"
-  "2023-12-13"
-  "2023-12-20"
-  "2023-12-27"
-  "2024-01-03"
-  "2024-01-10"
-  "2024-01-17"
-  "2024-01-24"
-  "2024-01-31"
-  "2024-02-07"
-  "2024-02-14"
-  "2024-02-21"
-  "2024-02-28"
-  "2024-03-06"
-  "2024-03-13"
-  "2024-03-20"
-  "2024-03-27"
-  "2024-04-03"
-  "2024-04-10"
-  "2024-04-17"
-  "2024-04-24"
-  "2024-05-01"
-  "2024-05-08"
-  "2024-11-27"
-  "2024-12-04"
-  "2024-12-11"
-  "2024-12-18"
-  "2024-12-25"
-  "2025-01-01"
-  "2025-01-08"
-  "2025-01-15"
-  "2025-01-22"
-  "2025-01-29"
-  "2025-02-05"
-  "2025-02-12"
-  "2025-02-19"
-  "2025-02-26"
-  "2025-03-05"
-  "2025-03-12"
-  "2025-03-19"
-  "2025-03-26"
-  "2025-04-02"
-  "2025-04-09"
-  "2025-04-16"
-  "2025-04-23"
-  "2025-04-30"
-  "2025-05-07"
-  "2025-05-14"
+  "2025-11-19"
+  "2025-11-26"
+  "2025-12-03"
+  "2025-12-10"
 )
 
 # Get the date for this array task
